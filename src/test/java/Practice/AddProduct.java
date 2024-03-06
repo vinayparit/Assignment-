19acaf53c8790d358e642af7486c775419cf8554
Admin <Admin@DESKTOP-OEB471V>
2024-03-06 17:25:30 +0530

    Added test script

---
+++ src/test/java/Practice/AddProduct.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class AddProduct {
+	
+	@Test
+	public void addProduct() {
+		System.out.println("product added");
+	}
+
+}
---
+++ src/test/java/Practice/DeleteProduct.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class DeleteProduct {
+	@Test
+	public void deleteProduct() {
+		System.out.println("product deleted");
+	}
+	
+
+}
