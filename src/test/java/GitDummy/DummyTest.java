608ab6b7cfb961ddd6db548632996784fab90f70
Admin <Admin@DESKTOP-Sharda>
2024-03-06 17:34:59 +0530

    commit2

---
+++ src/test/java/GitDummy/DummyTest.java
@@ -0,0 +1,16 @@
+package GitDummy;
+
+import org.testng.annotations.Test;
+
+public class DummyTest 
+{
+	@Test
+public void login()
+{
+	System.out.println("Login");
+}
+	public void signUp()
+	{
+		System.out.println("SignUp");
+	}
+}
