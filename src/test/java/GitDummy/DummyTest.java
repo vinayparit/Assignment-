c1ec4cb4d4106eb43ff79e7b0043b356f54b59ab
Admin <Admin@DESKTOP-Sharda>
2024-03-06 17:46:04 +0530

    Commiting

--- src/test/java/GitDummy/DummyTest.java
+++ src/test/java/GitDummy/DummyTest.java
@@ -9,8 +9,14 @@ public void login()
 {
 	System.out.println("Login");
 }
+	@Test
 	public void signUp()
 	{
 		System.out.println("SignUp");
+	}
+	@Test
+	public void logout()
+	{
+		System.out.println("LogOut");
 	}
 }
